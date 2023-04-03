647029c36c6fbfab748715646a522d374632190e
Jaimi Chong <46967996+JaiChong@users.noreply.github.com>
2023-04-03 14:30:36 -0700

    Add files via upload

---
+++ 01_unity_basics/reset_scene_with_alt_positions/Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // doesn't work
+    // void Start()
+    // {
+    //     Debug.Log("Start()");
+    //     if (Input.GetKeyDown("left"))
+    //     {
+    //         Debug.Log("left");
+    //         this.transform.position = new Vector3 (-7, 0, 0);
+    //     }
+    //     else if (Input.GetKeyDown("right"))
+    //     {
+    //         Debug.Log("right");
+    //         this.transform.position = new Vector3 (7, 0, 0);
+    //     }
+    // }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
---
+++ 01_unity_basics/reset_scene_with_alt_positions/Assets/ResetOnKeypress.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetOnKeypress : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R) && Input.GetKey("left") && !Input.GetKey("right"))
+        {
+            SceneManager.LoadScene("Level1_left");
+        }
+        else if (Input.GetKey(KeyCode.R) && Input.GetKey("right") && !Input.GetKey("left"))
+        {
+            SceneManager.LoadScene("Level1_right");
+        }
+        else if (Input.GetKey(KeyCode.R)) // || (Input.GetKey(KeyCode.R) && Input.GetKey("right") && Input.GetKey("left"))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
