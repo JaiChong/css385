f86505221f1f4f98ec93afa82c1d4f5ab1f3a375
Jaimi Chong <jaimi.chong@gmail.com>
2023-04-15 19:18:08 -0700

    Renamed unity_basics

---
+++ unity_basics/reset_scene_with_alt_positions/Assets/ResetOnFall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetOnFall : MonoBehaviour
+{
+    void OnTriggerEnter2D()
+    {
+        if (Input.GetKey("left") && !Input.GetKey("right"))
+        {
+            SceneManager.LoadScene("Level1_left");
+        }
+        else if (Input.GetKey("right") && !Input.GetKey("left"))
+        {
+            SceneManager.LoadScene("Level1_right");
+        }
+        else // ( (!Input.GetKey("left") && !Input.GetKey("right")) || ((Input.GetKey("left") && Input.GetKey("right")) )
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
